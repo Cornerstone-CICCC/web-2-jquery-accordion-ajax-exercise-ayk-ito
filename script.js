e4aebee15958d9be5aadb12bc069660a84005c47
Ayaka Ito <a.8.32.noko.info@gmail.com>
2024-11-27 12:00:08 -0800

    modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-$(function() {
+$(function () {
   // your code here
-})
+
+  // Exercise 1
+  $(".accordion-header").click(function () {
+    if ($(this).next().is(":visible")) {
+      $(".accordion-content").slideUp();
+    } else {
+      $(".accordion-content").slideUp();
+      $(this).next().slideDown();
+    }
+  });
+
+  // Exercise 2
+  $("button").click(function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function (response) {
+        let todos = response.todos;
+        let ul = $("ul");
+        todos.forEach((todo) => {
+          var li = $("<li></li>").text(todo.todo);
+          ul.append(li);
+        });
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  });
+});
